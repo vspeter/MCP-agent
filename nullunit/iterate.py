9d9730a5cc4d8800ee029a5f527f7b9fdf2424ff
Peter Howe <peter.howe@emc.com>
2015-07-02 11:35:22 -0600

    fixing bugs in mandeling reulsts in doTarget, doRequires

--- nullunit/iterate.py
+++ nullunit/iterate.py
@@ -6,13 +6,16 @@ import logging
 import socket
 from datetime import datetime
 
-from procutils import execute, execute_lines, NonZeroException
+from procutils import execute, execute_lines
 
 
 GIT_CMD = '/usr/bin/git'
 MAKE_CMD = '/usr/bin/make'
 WRK_DIR = '/nullunit'
 APT_GET_CMD = '/usr/bin/apt-get'
+
+def _makeDidNotthing( results ):
+  return len( results ) == 1 and ( results[0].startswith( 'make: Nothing to be done' ) or results[0].startswith( 'make: *** No rule to make' ) )
 
 def readState( file ):
   try:
@@ -30,7 +33,7 @@ def writeState( file, state ):
 def doStep( state, mcp, packrat ):
   start_state = state[ 'state' ]
   mcp.sendStatus( 'Executing Stage "%s"' % start_state )
-  logging.info( 'nullunit: Executing Stage "%s"' % start_state )
+  logging.info( 'iterate: Executing Stage "%s"' % start_state )
 
   if start_state == 'clone':
     state[ 'dir' ] = doClone( state )
@@ -54,7 +57,7 @@ def doStep( state, mcp, packrat ):
       mcp.setSuccess( False )
 
   mcp.sendStatus( 'Stage "%s" Complete' % start_state )
-  logging.info( 'nullunit: Stage "%s" Complete' % start_state )
+  logging.info( 'iterate: Stage "%s" Complete' % start_state )
 
 
 def doClone( state ):
@@ -68,69 +71,69 @@ def doClone( state ):
     else:
       raise e
 
+  logging.info( 'iterate: cloning "%s"' % state[ 'url' ] )
   execute( '%s clone %s' % ( GIT_CMD, state[ 'url' ] ), WRK_DIR )
   return glob.glob( '%s/*' % WRK_DIR )[0]
 
 
 def doCheckout( state ):
+  logging.info( 'iterate: checking out "%s"' % state[ 'branch' ] )
   execute( '%s checkout %s' % ( GIT_CMD, state[ 'branch' ] ), state[ 'dir' ] )
 
 
 def doRequires( state ):
+  logging.info( 'iterate: getting requires "%s"' % state[ 'requires' ] )
   env = os.environ
   env['DEBIAN_PRIORITY'] = 'critical'
   env['DEBIAN_FRONTEND'] = 'noninteractive'
+  results = execute_lines( '%s %s' % ( MAKE_CMD, state[ 'requires' ] ), state[ 'dir' ], env=env, ok_rc=[ 0, 2 ] )
 
-  try:
-    results = execute_lines( '%s %s' % ( MAKE_CMD, state[ 'requires' ] ), state[ 'dir' ], env=env )
-  except NonZeroException as e:
-    if results[0].startswith( 'make: *** No rule to make target' ):
-      return
-
-    raise e
-
-  if results[0].startswith( 'make: Nothing to be done' ):
+  if _makeDidNotthing( results ):
     return
 
   for required in results:
     required = required.strip()
     if not required:
       continue
-    logging.info( 'interate: installing "%s"' % required )
+
+    logging.info( 'iterate: installing "%s"' % required )
     execute( '%s install -y %s' % ( APT_GET_CMD, required ) )
 
 
 def doTarget( state, packrat, mcp ):
-  results = []
-  try:
-    results = execute_lines( '%s %s' % ( MAKE_CMD, state[ 'target' ] ), state[ 'dir' ] )
-  except NonZeroException:
-    mcp.setResults( '\n'.join( results ) )
-    return False
+  logging.info( 'iterate: executing target "%s"' % state[ 'target' ] )
+  results = execute_lines( '%s %s' % ( MAKE_CMD, state[ 'target' ] ), state[ 'dir' ], ok_rc=[ 0, 2 ] )
 
-  if results[0].startswith( 'make: Nothing to be done' ):
+  if _makeDidNotthing( results ):
     mcp.setResults( '' )
     return True
 
   mcp.setResults( '\n'.join( results ) )
 
   if state[ 'target' ] in ( 'dpkg', 'rpm', 'resource' ):
+    logging.info( 'iterate: getting package file "%s"' % state[ 'requires' ] )
     mcp.sendStatus( 'Package Build' )
-    file_name = os.path.join( state[ 'dir' ], execute_lines( '%s %s-file' % ( MAKE_CMD, state[ 'target' ] ), state[ 'dir' ] )[-1] )
-    file = open( file_name, 'r' )
-    try:
-      result = packrat.addPackageFile( file, 'Package File "%s"' % os.path.basename( file_name ), 'MCP Auto Build from %s.  Build on %s at %s' % ( state[ 'url' ], socket.getfqdn(), datetime.utcnow() ) )
-    except Exception as e:
-      logging.exception( 'iterate: Exception "%s" while adding package file "%s"' % ( e, file_name ) )
-      mcp.setResults( 'Exception adding package file' )
-      file.close()
-      return False
+    results = execute_lines( '%s %s-file' % ( MAKE_CMD, state[ 'target' ] ), state[ 'dir' ], ok_rc=[ 0, 2 ] )
+    if not results or _makeDidNotthing( results ):
+      raise Exception( 'package target did not return a file to upload' )
 
-    file.close()
-    if not result:
-      mcp.sendStatus( 'Packge NOT Uploaded' )
-      return False
+    for file_name in results:
+      file_name = os.path.join( state[ 'dir' ], file_name )
+      logging.info( 'iterate: uploading "%s"' % file_name )
+      src = open( file_name, 'r' )
+      try:
+        result = packrat.addPackageFile( src, 'Package File "%s"' % os.path.basename( file_name ), 'MCP Auto Build from %s.  Build on %s at %s' % ( state[ 'url' ], socket.getfqdn(), datetime.utcnow() ) )
+      except Exception as e:
+        logging.exception( 'iterate: Exception "%s" while adding package file "%s"' % ( e, file_name ) )
+        mcp.setResults( 'Exception adding package file' )
+        src.close()
+        return False
 
-    mcp.sendStatus( 'Package Uploaded' )
+      src.close()
+      if not result:
+        mcp.sendStatus( 'Packge(s) NOT (all) Uploaded' )
+        return False
+
+    mcp.sendStatus( 'Package(s) Uploaded' )
 
   return True
--- nullunit/procutils.py
+++ nullunit/procutils.py
@@ -1,14 +1,12 @@
 import subprocess
 import os
 import shlex
+import logging
 from datetime import datetime
 
 debug_stdout = None
 
 global_env = os.environ
-
-class NonZeroException( Exception ):
-  pass
 
 
 def open_output( filename ):
@@ -16,39 +14,58 @@ def open_output( filename ):
   debug_stdout = open( filename, 'w' )
 
 
-def _execute( cmd, dir, stdout, stderr, stdin, env ):
+def _execute( cmd, dir, stdout, stdin, env ):
+  logging.info( 'procutils: executing "%s" in "%s"' % ( cmd, dir ) )
   debug_stdout.write( '\n=================================================\n' )
   debug_stdout.write( '%s\n' % datetime.utcnow() )
   debug_stdout.write( 'Executing:\n' )
   debug_stdout.write( cmd )
+
   if stdin:
     debug_stdout.write( '\nstdin:\n' )
     debug_stdout.write( stdin )
+
   debug_stdout.write( '\n-------------------------------------------------\n' )
+  debug_stdout.flush()
+
+  args = {
+            'args': shlex.split( cmd ),
+            'cwd': dir,
+            'env': env,
+            'stdout': ( stdout if stdout is not None else debug_stdout ),
+            'stderr': subprocess.STDOUT
+         }
+
   try:
     if stdin:
-      proc = subprocess.Popen( shlex.split( cmd ), cwd=dir, env=env, stdout=stdout, stderr=stderr, stdin=subprocess.PIPE )
-      ( output, _ ) = proc.communicate( stdin )
+      proc = subprocess.Popen( stdin=subprocess.PIPE, **args )
+      ( results, _ ) = proc.communicate( stdin )
+
     else:
-      proc = subprocess.Popen( shlex.split( cmd ), cwd=dir, env=env, stdout=stdout, stderr=stderr )
-      ( output, _ ) = proc.communicate()
+      proc = subprocess.Popen( **args )
+      ( results, _ ) = proc.communicate()
+
   except Exception as e:
     raise Exception( 'Exception %s while executing "%s"' % ( e, cmd ) )
 
   debug_stdout.write( '\n-------------------------------------------------\n' )
   debug_stdout.write( 'rc: %s\n' % proc.returncode )
   debug_stdout.write( '%s\n' % datetime.utcnow() )
+  debug_stdout.flush()
 
-  if proc.returncode != 0:
-    raise NonZeroException( 'Error Executing "%s", rc: %s' % ( cmd, proc.returncode ) )
+  logging.info( 'procutils: returned "%s"' % proc.returncode )
 
-  return output
-
-
-def execute( cmd, dir=None, stdin=None, env=global_env ):
-  _execute( cmd, dir, debug_stdout, debug_stdout, stdin, env )
+  return ( results, proc.returncode )
 
 
-def execute_lines( cmd, dir=None, stdin=None, env=global_env ):
-  stdout = _execute( cmd, dir, subprocess.PIPE, debug_stdout, stdin, env )
-  return stdout.splitlines()
+def execute( cmd, dir=None, stdin=None, env=global_env, ok_rc=None ):
+  ( _, rc ) = _execute( cmd, dir, None, stdin, env )
+  if rc not in ( ok_rc if ok_rc is not None else [ 0 ] ):
+    raise Exception( 'Error Executing "%s", rc: %s' % ( cmd, rc ) )
+
+
+def execute_lines( cmd, dir=None, stdin=None, env=global_env, ok_rc=None ):
+  ( results, rc ) = _execute( cmd, dir, subprocess.PIPE, stdin, env )
+  if rc not in ( ok_rc if ok_rc is not None else [ 0 ] ):
+    raise Exception( 'Error Executing "%s", rc: %s' % ( cmd, rc ) )
+  return results.splitlines()
