28a878e053b90c2872e10bd81e4bedf5018eec98
Peter Howe <peter.howe@emc.com>
2015-07-15 23:04:56 -0600

    nullunitAddPAckageFile can handel distro versions

--- nullunit/Packrat.py
+++ nullunit/Packrat.py
@@ -16,9 +16,9 @@ class Packrat( object ):
   def _callback( self, pos, size ):
     logging.debug( 'Packrat: Uploading at %s of %s' % ( pos, size ) )
 
-  def addPackageFile( self, file, justification, provenance ):
+  def addPackageFile( self, file, justification, provenance, version=None ):
     logging.info( 'Packrat: Adding Packge File "%s"' % file.name )
     file_uri = self.cinp.uploadFile( '/api/FILES', file, self._callback )[ 'uri' ]
-    logging.info( 'Packrat: Adding file "%s", justification: "%s", provenance: "%s"' % ( file_uri, justification, provenance ) )
-    result = self.cinp.call( '/api/v1/Repos/PackageFile(create)', { 'file': file_uri, 'justification': justification, 'provenance': provenance }, timeout=120 ) # it can sometimes take a while for packrat to commit large files
+    logging.info( 'Packrat: Adding file "%s", justification: "%s", provenance: "%s", distro: "%s"' % ( file_uri, justification, provenance, version ) )
+    result = self.cinp.call( '/api/v1/Repos/PackageFile(create)', { 'file': file_uri, 'justification': justification, 'provenance': provenance, 'version': version }, timeout=120 ) # it can sometimes take a while for packrat to commit large files
     return result[ 'value' ]
