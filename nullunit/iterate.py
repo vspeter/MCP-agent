aa8b1302f024c762f8845cce30114c44ae4e9ae8
Peter Howe <peter.howe@emc.com>
2015-08-24 15:22:09 -0600

    fixed formatting bug

--- nullunit/iterate.py
+++ nullunit/iterate.py
@@ -176,7 +176,7 @@ def doTarget( state, mcp, config ):
     args.append( 'RESOURCE_INDEX=%s' % config.get( 'mcp', 'resource_index' ) )
 
   logging.info( 'iterate: executing target "%s"' % state[ 'target' ] )
-  ( target_results, rc ) = execute_lines_rc( '%s %s' % ( MAKE_CMD, state[ 'target' ], ' '.join( args ) ), state[ 'dir' ] )
+  ( target_results, rc ) = execute_lines_rc( '%s %s %s' % ( MAKE_CMD, state[ 'target' ], ' '.join( args ) ), state[ 'dir' ] )
 
   if rc != 0:
     if rc == 2 and _makeDidNothing( target_results ):
